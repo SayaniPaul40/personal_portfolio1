849eb69bb175fbdfd993739f75b38b5e2280148a
sayani <Sayani@8e86bfb0e784>
2020-07-19 07:25:25 +0000

    Remove old stuff

--- personal_portfolio1/settings.py
+++ personal_portfolio1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jxle=sokgd_kpxjc6^=z0-9+ukhuiso189u0iimrm-xst_xl!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Sayani.pythonanywhere.com"]
 
 
 # Application definition
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
